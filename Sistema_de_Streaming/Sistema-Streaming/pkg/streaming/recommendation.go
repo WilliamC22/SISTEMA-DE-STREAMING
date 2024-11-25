decb6e90de2ae20b979e3089dc44261c3dfe2fce
WilliamC22 <152014395+WilliamC22@users.noreply.github.com>
2024-11-24 23:14:14 -0500

    Update recommendation.go

--- Sistema_de_Streaming/Sistema-Streaming/pkg/streaming/recommendation.go
+++ Sistema_de_Streaming/Sistema-Streaming/pkg/streaming/recommendation.go
@@ -1,15 +1,17 @@
-package streaming
+package recommendation
 
 import (
+	"Sistema-Streaming/pkg/catalog"
 	"encoding/json"
 	"net/http"
 )
 
 // RecommendationHandler maneja las recomendaciones de contenido
 func RecommendationHandler(w http.ResponseWriter, r *http.Request) {
-	recommendations := []Content{
-		{ID: "3", Title: "Comedy Nights", Type: "movie"},
-		{ID: "4", Title: "Thrill Seekers", Type: "series"},
+	// Recomendaciones ficticias basadas en el catálogo
+	recommendations := []catalog.Content{
+		{ID: "3", Title: "Madagascar 2", Type: "movie"},
+		{ID: "4", Title: "Peaky Blinders", Type: "series"},
 	}
 
 	w.Header().Set("Content-Type", "application/json")
